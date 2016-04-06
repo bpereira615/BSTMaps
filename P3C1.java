790c0f7dd28dafc2e2f4709f4d013c2cc4339224
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2016-04-06 16:36:55 -0400

    Reverse frequency, word map change

--- P3C1.java
+++ P3C1.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class P3C1 {
 	public static void main(String[] args) {
@@ -37,70 +38,74 @@ public class P3C1 {
 
 
 
-        //hashmap of key-words, and vaule-frequency
-        HashMap<String, Integer> hashmap = new HashMap<>();
+        //wordToFreq of key-words, and vaule-frequency
+        HashMap<String, Integer> wordToFreq = new HashMap<>();
 
         //create map
         for (String w : words) {
-        	if (hashmap.containsKey(w)) {
+        	if (wordToFreq.containsKey(w)) {
         		//key is present (word has been seen), increment count
-        		hashmap.put(w, hashmap.get(w) + 1);
+        		wordToFreq.put(w, wordToFreq.get(w) + 1);
         	} else {
         		//word has not been see, initial put
-        		hashmap.put(w, 1);
+        		wordToFreq.put(w, 1);
         	}
+        }
+
+        //wordToFreq of key-frequency, and value - word
+        HashMap<Integer, ArrayList<String>> freqToWord = new HashMap<>();
+        
+        //frequency
+        Integer f;
+        for (String w : wordToFreq.keySet()) {
+            f = wordToFreq.get(w);
+            if (freqToWord.containsKey(f)) {
+                freqToWord.get(f).add(w);
+            } else {
+                ArrayList<String> wordList = new ArrayList<>();
+                wordList.add(w);
+                freqToWord.put(f, wordList);
+            }
         }
 
 
 
         //---------- total number of words in the document ----------
-        int numWords = words.size();
+        int numWords = wordToFreq.keySet().size();
         System.out.println("Total number of words: " + numWords);
+
+
 
 
         //---------- most frequent word(s) ----------
 
-        //array of frequencies, index corresponding to words array
-        ArrayList<Integer> freq = new ArrayList<>();
-        for (String w : words) {
-        	freq.add(hashmap.get(w));
-        }
-
-        int currIndex = 0;
-        ArrayList<Integer> maxIndex = new ArrayList<>();
-        Integer maxVal = new Integer(0);
-
-        for (Integer i : freq) {
-        	if (i.compareTo(maxVal) > 0) {
-        		maxVal = i;
-                //reset max values
-                maxIndex.clear();
-        		maxIndex.add(currIndex);
-        	} else if (i.compareTo(maxVal) == 0) {
-                maxIndex.add(currIndex);
+        //linear search, only searching once so not worth sorting
+        Integer max = new Integer(0);
+        for (Integer i : freqToWord.keySet()) {
+            if (i.compareTo(max) > 0) {
+                max = i;
             }
-        	currIndex ++;
         }
 
-        String s = "";
-        for (Integer i : maxIndex) {
-            s += words.get(i) + ", ";
-        }
-      	System.out.println("Most frequent word(s): " + s);
+
+
+        ArrayList<String> freqWords = freqToWord.get(max);
+      	System.out.println("Most frequent word(s): " + freqWords);
 
         //---------- all words that occurr at most three times ----------
-        s = "";
 
-        currIndex = 0;
-        for (Integer i : freq) {
+        //TODO: set for no duplicates, right?
+        HashSet<String> wordSet = new HashSet<>();
+        for (Integer i : freqToWord.keySet()) {
             if (i.compareTo(3) <= 0) {
-                s += words.get(currIndex) + ", ";
-            } 
-            currIndex++;
+                wordSet.addAll(freqToWord.get(i));
+            }
         }
 
+        System.out.println("Words occurring at most 3 times: " + wordSet);
+
        	//---------- words that occurr in top 10% of most frequent word ----------
-        
-        
+
+
 	}
 }
