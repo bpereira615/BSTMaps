8530e30d68e171133a9c5f3a293da19c9c0865f5
Ryan Demo <ryan.demo@me.com>
2016-04-10 21:13:03 -0400

    Added files via upload
    Both untested, but all features implemented and fully checkstyle compliant

--- src/P3C1.java
+++ src/P3C1.java
@@ -1,97 +1,149 @@
+/********************************************************************
+ * Benjamin Hoertnagl-Pereira 
+ * JHED: bhoertn1 
+ * 631-488-7197 
+ * bhoertn1@jhu.edu
+ * 
+ * Ryan Demo
+ * JHED: rdemo1
+ * rdemo1@jhu.edu
+ *
+ * 600.226.02 | CS226 Data Structures | Section 2
+ * Project 3, Part C1 - Fun with Maps
+ *
+ *******************************************************************/
+
+
 import java.util.Scanner;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
-public class P3C1 {
-	public static void main(String[] args) {
-		//create scanner from file
-		Scanner infile = null;
-        boolean inerror = false;
+/** Analyzes and sorts word frequencies given a list of words.
+ */
+public final class P3C1 {
 
-        try {
-            System.out.println("0 " + args[0]
-                    + " should be input filename.");
-            infile = new Scanner(new FileReader(args[0]));
-        } catch (ArrayIndexOutOfBoundsException a) {
-            System.err.println("Must give input filename at command line.");
-            inerror = true;
-        } catch (IOException f) {
-            System.err.println("Can't open that file, try again.");
-            inerror = true;
-        }
-        if (inerror) {
-            System.err.println("Exiting...");
-            System.exit(1);
-        }
+    /** The constant 10.
+     */
+    static final int TEN = 10;
 
+    /** Dummy constructor for utility class.
+     */
+    private P3C1() {
+    }
+
+    /** Given a list of words in System.in, runs frequency analysis
+     * method on the words.
+     * @param args command line arguments, unused
+     */
+    public static void main(String[] args) {
+
+        Scanner cnsl = new Scanner(System.in);
 
         //array of words
         ArrayList<String> words = new ArrayList<>();
 
         //insert words into array before start
-        while(infile.hasNext()) {
-        	words.add(infile.next());
+        while (cnsl.hasNext()) {
+            words.add(cnsl.next());
         }
 
+        cnsl.close();
 
-
-        //hashmap of key-words, and vaule-frequency
-        HashMap<String, Integer> hashmap = new HashMap<>();
+        //wordToFreq of key-words, and value-frequency
+        HashMap<String, Integer> wordToFreq = new HashMap<>();
 
         //create map
         for (String w : words) {
-        	if (hashmap.containsKey(w)) {
-        		//key is present (word has been seen), increment count
-        		hashmap.put(w, hashmap.get(w) + 1);
-        	} else {
-        		//word has not been see, initial put
-        		hashmap.put(w, 1);
-        	}
+            if (wordToFreq.containsKey(w)) {
+                //key is present (word has been seen), increment count
+                wordToFreq.put(w, wordToFreq.get(w) + 1);
+            } else {
+                //word has not been see, initial put
+                wordToFreq.put(w, 1);
+            }
         }
 
+        //wordToFreq of key-frequency, and value - word
+        HashMap<Integer, ArrayList<String>> freqToWord = new HashMap<>();
 
+        //frequency (reverse index)
+        Integer f;
+        for (String w : wordToFreq.keySet()) {
+            f = wordToFreq.get(w);
+            if (freqToWord.containsKey(f)) {
+                freqToWord.get(f).add(w);
+            } else {
+                ArrayList<String> wordList = new ArrayList<>();
+                wordList.add(w);
+                freqToWord.put(f, wordList);
+            }
+        }
+
+        freqAnalysis(wordToFreq, freqToWord);
+
+    }
+
+    /** Takes two HashMaps: word to frequency and frequency to words (its
+     * reverse), printing total number of words, most frequent words,
+     * all words that occur at most three times, and all words that
+     * occur within the top 10% of the most frequent words.
+     * @param wordToFreq HashMap of (word, frequency) pairs
+     * @param freqToWord HashMap of (frequency, words[]) pairs
+     */
+    public static void freqAnalysis(HashMap<String, Integer> wordToFreq, 
+            HashMap<Integer, ArrayList<String>> freqToWord) {
 
         //---------- total number of words in the document ----------
-        int numWords = words.size();
+        int numWords = wordToFreq.size();
         System.out.println("Total number of words: " + numWords);
 
 
         //---------- most frequent word(s) ----------
 
-        //array of frequencies, index corresponding to words array
-        ArrayList<Integer> freq = new ArrayList<>();
-        for (String w : words) {
-        	freq.add(hashmap.get(w));
-        }
-
-        int currIndex = 0;
-        ArrayList<Integer> maxIndex = new ArrayList<>();
-        Integer maxVal = new Integer(0);
-
-        for (Integer i : freq) {
-        	if (i.compareTo(maxVal) > 0) {
-        		maxVal = i;
-                //reset max values
-                maxIndex.clear();
-        		maxIndex.add(currIndex);
-        	} else if (i.compareTo(maxVal) == 0) {
-                maxIndex.add(currIndex);
+        //linear search, only searching once so not worth sorting
+        Integer max = new Integer(0);
+        for (Integer i : freqToWord.keySet()) {
+            if (i.compareTo(max) > 0) {
+                max = i;
             }
-        	currIndex ++;
         }
 
-        String s = "";
-        for (Integer i : maxIndex) {
-            s += words.get(i) + ", ";
+        ArrayList<String> freqWords = freqToWord.get(max);
+        System.out.println("Most frequent word(s): " + freqWords);
+
+
+        //---------- all words that occur at most three times ----------
+
+        //using set to not have duplicate words
+        HashSet<String> wordSet = new HashSet<>();
+        for (Integer i : freqToWord.keySet()) {
+            if (i.compareTo(2 + 1) <= 0) {
+                wordSet.addAll(freqToWord.get(i));
+            }
         }
-      	System.out.println("Most frequent word(s): " + s);
 
-        //---------- all words that occurr at most three times ----------
+        System.out.println("Words occurring at most 3 times: " + wordSet);
 
 
-       	//---------- words that occurr in top 10% of most frequent word ----------
+        //----- words that occur in top 10% of most frequent words -----
 
-	}
+        ArrayList<String> top10FreqWords = new ArrayList<String>();
+
+        ArrayList<Integer> sortedKeys =
+                new ArrayList<Integer>(freqToWord.keySet());
+        Collections.sort(sortedKeys);
+
+        int size = wordToFreq.size() / TEN; //integer division
+        int index = sortedKeys.size() - 1; //start from end of array
+        while (top10FreqWords.size() <= size) {
+            top10FreqWords.addAll(freqToWord.get(sortedKeys.indexOf(index)));
+            index--; //decrement every time to get lower frequency words
+        }
+
+        System.out.println("Top 10% of most frequent words: " + top10FreqWords);
+
+    }
+
 }
--- src/P3C2.java
+++ src/P3C2.java
@@ -1,115 +1,153 @@
+/********************************************************************
+ * Benjamin Hoertnagl-Pereira 
+ * JHED: bhoertn1 
+ * 631-488-7197 
+ * bhoertn1@jhu.edu
+ * 
+ * Ryan Demo
+ * JHED: rdemo1
+ * rdemo1@jhu.edu
+ *
+ * 600.226.02 | CS226 Data Structures | Section 2
+ * Project 3, Part C2 - Fun with Maps
+ *
+ *******************************************************************/
+
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.TreeMap;
 import java.util.ArrayList;
-import java.io.IOException;
-import java.io.FileReader;
 import java.util.Scanner;
 
-public class P3C2 {
-	public static void main(String[] args) {
-		//create scanner from file
-		Scanner infile = null;
-        boolean inerror = false;
+/** Class to analyze insertion times of HashMap, BSTMap, and TreeMap
+ * at different increments of data to insert.
+ */
+public final class P3C2 {
+    
+    
+    /** Constant 100.
+     */
+    static final int ONE_HUNDRED = 100;
+    
+    /** Constant 1000.
+     */
+    static final int ONE_THOUSAND = 1000;
+    
+    /** Constant 100,000.
+     */
+    static final int ONE_HUNDRED_THOUSAND = 100000;
+    
+    
+    /** Dummy constructor for utility class.
+     */
+    private P3C2() {
+    }
+    
+    /** Takes in list of words from console and analyzes their insertion
+     * into a HashMap, BSTMap and TreeMap at the 100, 1000, and 100000
+     * word levels.
+     * @param args command line arguments, unused
+     */
+    public static void main(String[] args) {
 
-        try {
-            System.out.println("0 " + args[0]
-                    + " should be input filename.");
-            infile = new Scanner(new FileReader(args[0]));
-        } catch (ArrayIndexOutOfBoundsException a) {
-            System.err.println("Must give input filename at command line.");
-            inerror = true;
-        } catch (IOException f) {
-            System.err.println("Can't open that file, try again.");
-            inerror = true;
-        }
-        if (inerror) {
-            System.err.println("Exiting...");
-            System.exit(1);
-        }
-
+        Scanner cnsl = new Scanner(System.in);
 
         //array of words
         ArrayList<String> words = new ArrayList<>();
 
         //insert words into array before start
-        while(infile.hasNext()) {
-        	words.add(infile.next());
+        while (cnsl.hasNext()) {
+            words.add(cnsl.next());
         }
-
-
-
+        
+        cnsl.close();
+        
+        if (words.size() >= ONE_HUNDRED) {
+            System.out.println("First 100 Words - Implementation Times");
+            analyzeList(words.subList(0, ONE_HUNDRED));
+            System.out.println();
+        } else {
+            System.out.println("Insufficient words for analysis.");
+        }
+        
+        if (words.size() >= ONE_THOUSAND) {
+            System.out.println("First 1000 Words - Implementation Times");
+            analyzeList(words.subList(0, ONE_THOUSAND));
+            System.out.println();
+        } 
+        
+        if (words.size() >= ONE_HUNDRED_THOUSAND) {
+            System.out.println("First 100,000 Words - Implementation Times");
+            analyzeList(words.subList(0, ONE_HUNDRED_THOUSAND));
+            System.out.println();
+        }
+        
+    }
+    
+    /** Takes a list of strings and inserts its contents into a
+     * HashMap, BSTMap, and TreeMap then prints insertion times.
+     * @param words list of words to analyze insertion times
+     */
+    public static void analyzeList(List<String> words) {
         //various map implementations
         HashMap<String, Integer> hashmap = new HashMap<>();
         BSTMap<String, Integer> bstmap = new BSTMap<>();
         TreeMap<String, Integer> avlmap = new TreeMap<>();
 
-
-
+        
         //--------------- Hashmap  ---------------
         long lStartTime = System.currentTimeMillis();
 
         //create map
         for (String w : words) {
-        	if (hashmap.containsKey(w)) {
-        		//key is present (word has been seen), increment count
-        		hashmap.put(w, hashmap.get(w) + 1);
-        	} else {
-        		//word has not been see, initial put
-        		hashmap.put(w, 1);
-        	}
+            if (hashmap.containsKey(w)) {
+                //key is present (word has been seen), increment count
+                hashmap.put(w, hashmap.get(w) + 1);
+            } else {
+                //word has not been see, initial put
+                hashmap.put(w, 1);
+            }
         }
 
         long lEndTime = System.currentTimeMillis();
-		long difference = lEndTime - lStartTime;
-		System.out.println("HashMap - elapsed milliseconds: " + difference);
+        long difference = lEndTime - lStartTime;
+        System.out.println("HashMap - elapsed milliseconds: " + difference);
 
 
-
-
-
-
-
-
-
-		//--------------- BSTMap ---------------
+        //--------------- BSTMap ---------------
         lStartTime = System.currentTimeMillis();
 
         //create map
         for (String w : words) {
-        	if (bstmap.hasKey(w)) {
-        		bstmap.put(w, bstmap.get(w) + 1);
-        	} else {
-        		bstmap.put(w, 1);
-        	}
+            if (bstmap.hasKey(w)) {
+                bstmap.put(w, bstmap.get(w) + 1);
+            } else {
+                bstmap.put(w, 1);
+            }
         }
 
 
         lEndTime = System.currentTimeMillis();
-		difference = lEndTime - lStartTime;
-		System.out.println("BSTMap - elapsed milliseconds: " + difference);
+        difference = lEndTime - lStartTime;
+        System.out.println("BSTMap - elapsed milliseconds: " + difference);
 
 
-
-
-
-
-
-
-		//--------------- AVLMap ---------------
+        //--------------- AVLMap ---------------
         lStartTime = System.currentTimeMillis();
 
         //create map
         for (String w : words) {
-        	if (avlmap.containsKey(w)) {
-        		avlmap.put(w, avlmap.get(w) + 1);
-        	} else {
-        		avlmap.put(w, 1);
-        	}
+            if (avlmap.containsKey(w)) {
+                avlmap.put(w, avlmap.get(w) + 1);
+            } else {
+                avlmap.put(w, 1);
+            }
         }
 
         lEndTime = System.currentTimeMillis();
-		difference = lEndTime - lStartTime;
-		System.out.println("AVLMap - elapsed milliseconds: " + difference);
-
-	}
+        difference = lEndTime - lStartTime;
+        System.out.println("AVLMap - elapsed milliseconds: " + difference);
+    }
+    
 }
